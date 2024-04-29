f09e45438aaefe1534de0575382c12347b8070d0
megamonsta2 <sam9kermanshah@gmail.com>
2024-04-29 17:10:34 +0100

    added temp main func

--- MEGA SCRIPT/main.py
+++ MEGA SCRIPT/main.py
@@ -1,2 +1,19 @@
 from GetCadets import getCadets
-print(getCadets())
+
+outputFiles = {
+    "Usernames": open("Outputs/Usernames.txt", "w")
+}
+
+usernames = getCadets()
+
+def main():
+
+    # write usernames
+    file = outputFiles["Usernames"]
+
+    for _, username in enumerate(usernames):
+        file.write(username + "\n")
+    file.close()
+
+if __name__ == "__main__":
+    main()
