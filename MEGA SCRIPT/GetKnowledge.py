fdb689734b622b1a19293d2ab4a12b024f90920f
megamonsta2 <sam9kermanshah@gmail.com>
2024-04-29 18:50:56 +0100

    changed var name and a comment

--- MEGA SCRIPT/GetKnowledge.py
+++ MEGA SCRIPT/GetKnowledge.py
@@ -6,20 +6,20 @@ inputScores = open(inputFolder + "/knowledgeScores.txt", "r").readlines()
 
 def GetKnowledge(usernames, missing):
     for key, name in enumerate(inputNames):
+
         name = name.strip()
-        
-        scoreToAdd = int(inputScores[key].strip())
+        score = int(inputScores[key].strip())
 
         if name.lower() in usernames: # player is an AT
             plrData = usernames[name.lower()]
 
-            if ("Written" not in plrData) or (plrData["Written"] > scoreToAdd): # score doesnt exist OR existing score is greater than new score
-                plrData["Written"] = scoreToAdd # assign new score
+            if ("Written" not in plrData) or (plrData["Written"] > score): # score doesnt exist OR existing score is greater than new score
+                plrData["Written"] = score # assign new score
         
-        else: # missing users
+        else: # not AT
             if name.lower() not in missing: # new missing data
-                missing[name.lower()] = {"Username": name, "Written": scoreToAdd}
+                missing[name.lower()] = {"Username": name, "Written": score}
             else: # has missing data
-                missing[name.lower()]["Written"] = scoreToAdd
+                missing[name.lower()]["Written"] = score
     
     # return usernames, missing
