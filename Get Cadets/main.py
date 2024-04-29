c73793512071d30442266cb743026ecba94b3f69
megamonsta2 <sam9kermanshah@gmail.com>
2024-04-29 16:49:22 +0100

    reworked get cadets

---
+++ Get Cadets/main.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+groupID = "8536935"
+roleID = "107675583" # AT
+
+def main():
+    writeToFile(getCadets())
+
+def getCadets():
+    usernames = []
+    nextCursor = ""
+
+    while True:
+        url = "https://groups.roblox.com/v1/groups/" + groupID + "/roles/" + roleID + "/users?limit=100&sortOrder=Desc&cursor=" + nextCursor + "&"
+        
+        parsedData = json.loads(requests.get(url).text)
+        for _, plrData in enumerate(parsedData["data"]):
+            usernames.append(plrData["username"])
+
+        if not parsedData["nextPageCursor"]:
+            break
+        else:
+            nextCursor = parsedData["nextPageCursor"]
+    
+    print(usernames)
+    return usernames
+
+def writeToFile(usernames):
+    file = open("output.txt", "w")
+
+    for _, username in enumerate(usernames):
+        file.write(username + "\n")
+    file.close()
+
+if __name__ == "__main__":
+    main()
