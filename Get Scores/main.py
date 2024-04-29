30171d7eae315f80774e28cfa423e3e71b0f25a6
megamonsta2 <sam9kermanshah@gmail.com>
2024-04-29 23:43:49 +0100

    Added writing to files

--- Get Scores/main.py
+++ Get Scores/main.py
@@ -7,12 +7,13 @@ outputFolder = "Outputs"
 outputFiles = {
     "Username": True,
     "Knowledge": True,
-    "Musket": True,
     "Dummies": True,
     "Speed": True,
+    "Musket": True,
     "TT": True,
     "Bonus": True,
 }
+missingFiles = open(outputFolder + "/Missing.txt", "w")
 
 def main():
     InitialiseOutputFiles()
@@ -27,12 +28,51 @@ def main():
 
     print(scores)
     print(missing)
+
+    WriteToFiles(scores)
+    WriteToMissing(missing)
+    CloseFiles()
     
     input()
 
 def InitialiseOutputFiles():
     for key in outputFiles:
         outputFiles[key] = open(outputFolder + "/" + key + ".txt", "w")
+
+def WriteToFiles(scores):
+    for name in scores:
+        plrData = scores[name]
+
+        for test in outputFiles:
+            file = outputFiles[test]
+
+            if test in plrData:
+                file.write(str(plrData[test]) + "\n")
+            else:
+                file.write("\n")
+
+writeMissingOrder = ["Username",  "Knowledge", "Dummies", "Speed", "Musket", "TT", "Bonus"]
+def WriteToMissing(missing):
+    file = missingFiles
+
+    for name in missing:
+        plrData = missing[name]
+
+        toWrite = ""
+        for _, test in enumerate(writeMissingOrder):
+            toWrite += test + ": "
+            if plrData[test]:
+                toWrite += str(plrData[test])
+            else:
+                toWrite += "N/A"
+            toWrite += test + " | "
+        file.write(toWrite)
+
+def CloseFiles():
+    for file in outputFiles:
+        outputFiles[file].close()
+    missingFiles.close()
+
 
 # def writeUsernames():
 #     file = outputFiles["Usernames"]
