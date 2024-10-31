09f0d0667f5a6274466bff8d2f4a44a79efcb204
megamonsta2 <sam9kermanshah@gmail.com>
2024-10-31 14:31:00 +0000

    created Sort script

---
+++ src/Sort.ts
@@ -0,0 +1,123 @@
+// Modules
+import { writeFile } from "fs/promises";
+import { SendErrors, FormatString, ReadFile } from "./Utils.js";
+import { OUTPUT_FOLDER } from "./Constants.js";
+
+// Variables
+const INPUT_FILE = "Unsorted";
+const OUTPUT_FILE = `${OUTPUT_FOLDER}/Sorted.txt`;
+const REGEX_PATTERN = /[A-z0-9_]+ - ([0-9]+) \(.+\)/;
+const MAX_SCORE = 100;
+
+const SortedScores: Map<number, string[]> = new Map();
+
+export default async () => {
+  await SortScores();
+  await OutputScores();
+};
+
+async function SortScores() {
+  const Errors: string[] = [];
+  let InputData = await ReadFile(INPUT_FILE);
+
+  while (true) {
+    let AllValid = true;
+
+    for (let index = 0; index < InputData.length; index++) {
+      // Get vars
+      const line = InputData[index];
+      const line_number = index + 1;
+
+      // Ignore empty strings
+      if (line == "") {
+        continue;
+      }
+
+      // Get parsed data
+      const result = ValidateLine(line);
+
+      // If valid, add to scores
+      if (result[0]) {
+        const score = result[1];
+        let ScoreArray: string[];
+
+        // Check if array exists for score
+        if (SortedScores.has(score)) {
+          ScoreArray = SortedScores.get(score) as string[];
+        } else {
+          ScoreArray = [];
+          SortedScores.set(score, ScoreArray);
+        }
+
+        ScoreArray.push(line);
+      } else {
+        // Otherwise add to error messages
+        Errors.push(FormatString(result[1], line_number, MAX_SCORE));
+        AllValid = false;
+      }
+    }
+
+    // Check if valid
+    if (!AllValid) {
+      await SendErrors("Sorted Scores", Errors);
+
+      // Recalculate vars
+      InputData = await ReadFile(INPUT_FILE);
+      SortedScores.clear();
+      Errors.length = 0;
+    } else {
+      break;
+    }
+  }
+}
+
+export function ValidateLine(line: string): [true, number] | [false, string] {
+  const PatternResult = REGEX_PATTERN.exec(line);
+  if (!PatternResult) {
+    return [false, `Data on line number {0} is not valid score data.`];
+  }
+
+  const score = Number(PatternResult[1]);
+
+  if (isNaN(score)) {
+    return [false, "The score on line {0} is not a number."];
+  }
+
+  if (!Number.isInteger(score)) {
+    return [false, "The score on line {0} is not an integer (whole number)."];
+  }
+
+  if (score > MAX_SCORE) {
+    return [
+      false,
+      "The score on line {0} is greater than the maximum score ({1}).",
+    ];
+  }
+
+  if (score < 0) {
+    return [false, "The score on line {0} is less than 0."];
+  }
+
+  return [true, score];
+}
+
+async function OutputScores() {
+  const Output: string[] = [];
+
+  for (let i = 100; i >= 0; i--) {
+    // Get scores
+    const Scores = SortedScores.get(i);
+
+    // Skip if no data
+    if (!Scores) {
+      continue;
+    }
+
+    // Add each score to output
+    for (const line of Scores) {
+      Output.push(line);
+    }
+  }
+
+  await writeFile(OUTPUT_FILE, Output.join("\r\n"), "utf8");
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@
 import { question } from "readline-sync";
 import { Create as CreateFiles, Clear as ClearFiles } from "./Files.js";
 import { index as OrderScores } from "./OrderScores/index.js";
+import Sort from "./Sort.js";
 
 // Variables
 const QUESTION =
-  "What action?\r\n1. Create Files\r\n2. Clear Files\r\n3. Order Scores\r\n4. Quit\r\n> ";
+  "What action?\r\n1. Create Files\r\n2. Clear Files\r\n3. Order Scores\r\n4. Sort Scores\r\n5. Quit\r\n> ";
 
 // Module Setup
 async function main() {
@@ -19,6 +20,8 @@ async function main() {
     } else if (answer == "3") {
       await OrderScores();
     } else if (answer == "4") {
+      await Sort();
+    } else if (answer == "5") {
       process.exit();
     } else {
       console.log("Invalid input!");
